ec23b6d0c2bad74b81c23c8aafa4ac7b2b7e9ac5
DJ Neill <dj.neill856@gmail.com>
2023-02-11 02:20:28 -0600

    Add files via upload
    adding reverse string challenge

--- codewars/js/main.js
+++ codewars/js/main.js
@@ -66,3 +66,13 @@ function evenOrOdd(number) {
     return 'Odd'
   }
 }
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  return str.split('').reverse().join('').split(' ').reverse().join(' ')
+}
