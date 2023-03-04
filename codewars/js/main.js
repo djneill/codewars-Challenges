9c5cb4697d029176c8d3618f3ac578afbc7b8a71
DJ Neill <dj.neill856@gmail.com>
2023-03-04 02:47:02 -0600

    Add files via upload

--- codewars/js/main.js
+++ codewars/js/main.js
@@ -131,3 +131,9 @@ function cockroachSpeed(s) {
   s = s * 27.7778
   return Math.floor(s)
 }
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  s = s.replace(/!/g, "")
+  return s
+}
