ec7568ce80e019af5275ba678925c66a9901461d
DJ Neill <dj.neill856@gmail.com>
2023-03-07 01:43:27 -0600

    remove exclamtion mark

--- codewars/js/main.js
+++ codewars/js/main.js
@@ -119,18 +119,6 @@ function validatePIN (pin) {
   return Number.isInteger(+pin) && [4, 6].includes(pin.length)
 }
 
-//The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
-
-// For example:
-
-// 1.08 --> 30
-// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
-
-function cockroachSpeed(s) {
-  //Good Luck!
-  s = s * 27.7778
-  return Math.floor(s)
-}
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 function removeExclamationMarks(s) {
