5d6cb50f3340d4fc26b05386d94aafc69b81bac0
DJ Neill <dj.neill856@gmail.com>
2023-03-09 03:11:22 -0600

    add new challenge for main.js

--- codewars/js/main.js
+++ codewars/js/main.js
@@ -110,10 +110,3 @@ const stringToNumber = function(str){
 function getAge(inputString){
   return parseInt(inputString)
 }
-
-// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-
-function removeExclamationMarks(s) {
-  s = s.replace(/!/g, "")
-  return s
-}
