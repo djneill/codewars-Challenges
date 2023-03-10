2878d502dcbf1cbe8fd7fc52f65e5a449b2ac000
DJ Neill <dj.neill856@gmail.com>
2023-03-10 03:49:00 -0600

    new challenge for main.js

--- codewars/js/main.js
+++ codewars/js/main.js
@@ -92,21 +92,3 @@ function boolToWord( bool ){
 function smash (words) {
   return words.join(' ')
 }
-
-// We need a function that can transform a string into a number. What ways of achieving this do you know?
-
-// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
-
-const stringToNumber = function(str){
-  return Number(str)
-}
-
-// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
-
-// Write a program that returns the girl's age (0-9) as an integer.
-
-// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
-
-function getAge(inputString){
-  return parseInt(inputString)
-}
